fb691420858d39527307d47bb238b2f5466dfacf
bkhurley <hurley.brian@gmail.com>
2018-01-14 15:41:39 -0800

    py script for tracking which stations are on the PITT line

---
+++ pitt_line_stations.py
@@ -0,0 +1,11 @@
+'''
+pitt_line_stations.py
+
+List of station abbreviations for desired line(s)
+
+11 Jan 2018 BH
+'''
+
+stations = ['MCAR', '19TH', '12TH', 'PHIL', 'WCRK', 'LAFY', 'ORIN', 'ROCK', 
+'WOAK', 'EMBR', 'MONT', 'POWL', 'CIVC', '16TH', '24TH', 'GLEN', 'BALB', 
+'DALY', 'COLM', 'NCON', 'PITT', 'SSAN', 'SBRN', 'SFIA', 'MLBR']
