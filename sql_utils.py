080975b16bcb6a53f2a1ff391e4effac5f24d3ce
bkhurley <hurley.brian@gmail.com>
2018-01-30 10:55:53 -0800

    some updates to general utility functions for SQL querying

--- sql_utils.py
+++ sql_utils.py
@@ -7,19 +7,35 @@ import pandas as pd
 import psycopg2
 
 def pg2pd_join(table1, table2, on1, on2):
-	'''
-	select and join data from two postgres tables
-	return as pandas dataframe
-	'''
-	# open db connection
-	dbname = 'bart_db'
-	username = 'bkhurley'
-	con = None
-	con = psycopg2.connect(database=dbname, user=username)
+    '''
+    select and join data from two postgres tables
+    return as pandas dataframe
+    '''
+    # open db connection
+    dbname = 'bart_db'
+    username = 'bkhurley'
+    con = None
+    con = psycopg2.connect(database=dbname, user=username)
 
-	# query the data & store in a dataframe
-	sql_query = "SELECT * FROM %s INNER JOIN %s ON (%s = %s);" % (table1, table2, on1, on2)
+    # query the data & store in a dataframe
+    sql_query = "SELECT * FROM %s INNER JOIN %s ON (%s = %s);" % (table1, table2, on1, on2)
+    join_df = pd.read_sql_query(sql_query,con)
+    con.close() #close dn connection
+    return join_df
 
-	join_df = pd.read_sql_query(sql_query,con)
-	con.close() #close dn connection
-	return join_df
+
+def pg2pd_join_two_cols(table1, table2, on1, on2):
+    '''
+    join tables on two columns
+    on1 and on2 should be lists containing the two column names
+    '''
+    # open db connection
+    dbname = 'bart_db'
+    username = 'bkhurley'
+    con = None
+    con = psycopg2.connect(database=dbname, user=username)
+    # query db and return in a df
+    sql_query = "SELECT * FROM %s INNER JOIN %s ON (%s = %s AND %s = %s);" % (table1, table2, on1[0], on2[0], on1[1], on2[1])
+    join_twocol_df = pd.read_sql_query(sql_query,con)
+    con.close() #close dn connection
+    return join_twocol_df
